4128bddeeb643059040aad64898501e62c068aba
tpbstats <ayeytvhh@sharklasers.com>
2014-12-13 10:10:21 +0100

    ready

--- scrape.go
+++ scrape.go
@@ -14,14 +14,12 @@ func scrape(db gorm.DB) {
 
 	log.Println("Commencing")
 
-	// Get urls
 	urls := make([]string, 10)
 	base := "https://torrentz.eu/search?f=movie&p="
 	for i := 0; i < len(urls); i++ {
 		urls[i] = fmt.Sprintf("%s%d", base, i)
 	}
 
-	// Get responses
 	var responses = make([]*http.Response, len(urls))
 	for key, url := range urls {
 		log.Printf("Getting %s", url)
@@ -34,7 +32,6 @@ func scrape(db gorm.DB) {
 		log.Printf("Success %s", url)
 	}
 
-	// Get statuses
 	log.Println("Statuses")
 	statuses := make(map[string]Status)
 	for _, response := range responses {
@@ -45,30 +42,24 @@ func scrape(db gorm.DB) {
 		}
 		lists := document.Find(".results dl:not(:last-of-type)")
 		lists.Each(func(i int, list *goquery.Selection) {
-			hash, _ := list.Find("dt a").Attr("href")
-			hash = hash[1:]
-			seeders := stringToInt(list.Find("dd span.u").Text())
-			leechers := stringToInt(list.Find("dd span.d").Text())
-			status := Status{
-				Hash:     hash,
-				Seeders:  seeders,
-				Leechers: leechers,
-			}
-			statuses[hash] = status
+			href, _ := list.Find("dt a").Attr("href")
+			status := Status{}
+			status.Hash = href[1:]
+			status.Seeders = stringToInt(list.Find("dd span.u").Text())
+			status.Leechers = stringToInt(list.Find("dd span.d").Text())
+			statuses[status.Hash] = status
 		})
 	}
 
-	// Assemble scrape
+	log.Println("Assembling")
 	scrape := Scrape{
 		Time:     time.Now(),
 		Statuses: make([]Status, 0, len(statuses)),
 	}
 	for _, status := range statuses {
-        scrape.Statuses = append(scrape.Statuses, status)
-    }
+		scrape.Statuses = append(scrape.Statuses, status)
+	}
 
-	// Insert scrape
-	db.LogMode(true)
 	log.Println("Inserting")
 	db.Create(&scrape)
 	log.Printf("Inserted, id=%d", scrape.Id)
